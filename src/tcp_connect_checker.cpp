23bec648f2c7cb0fd38de78023c4a5fc4246b7cf
alex <alex@staticlibs.net>
2018-02-08 21:46:02 +0000

    ci integration, license headers

--- src/tcp_connect_checker.cpp
+++ src/tcp_connect_checker.cpp
@@ -1,5 +1,6 @@
 /*
  * Copyright 2015, akashche at redhat.com
+ * Copyright 2017, alex at staticlibs.net
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
